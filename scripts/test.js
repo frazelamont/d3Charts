7434e8402a5dddf8ce65921c96222ee7637668e1
Fraser Lamont <fraser.lamont@live.co.uk>
2020-02-26 15:37:28 +0000

    d3 js functions

---
+++ scripts/test.js
@@ -0,0 +1,81 @@
+/*
+ * show a circle
+ * parameters:
+ * - group (svg element)
+ * - radius (int)
+ * - colour (string)
+ * - xAxis (int)
+ * - yAxis (int)
+ */
+            function drawCircle(group, radius, colour, xAxis, yAxis)
+            {
+                group
+                  .append("circle")
+                  .attr("fill", colour)
+                  .attr("r", radius)
+                  .attr("cx", xAxis)
+                  .attr("cy", yAxis)
+                ;
+            }
+
+/*
+ * show a proportional circle
+ * parameters:
+ * - group (svg element)
+ * - radius (int)
+ * - colour (string)
+ * - xAxis (int)
+ * - yAxis (int)
+ */
+function drawProportionalCircle(group, radius, colour, xAxis, yAxis)
+{
+    group
+      .append("circle")
+      .attr("fill", colour)
+      .attr("r", Math.sqrt(radius))
+      .attr("cx", xAxis)
+      .attr("cy", yAxis)
+    ;
+}
+
+            /*
+             * show a rectangle
+             * parameters:
+             * - group (svg element)
+             * - colour (string)
+             * - height (int)
+             * - width (int)
+             * - xAxis (int)
+             * - yAxis (int)
+             */
+            function drawRectangle(group, colour, height, width, xAxis, yAxis)
+            {
+                group
+                  .append("rect")
+                  .attr("fill", colour)
+                  .attr("width", width)
+                  .attr("height", height)
+                  .attr("x", xAxis)
+                  .attr("y", yAxis)
+                ;
+            }
+
+            /*
+             * show a text area
+             * parameters:
+             * - group (svg element)
+             * - text (string)
+             * - size (string)
+             * - xAxis (int)
+             * - yAxis (int)
+             */
+            function drawText(group, text, size, xAxis, yAxis)
+            {
+                group
+                  .append("text")
+                  .text(text)
+                  .attr("x", xAxis)
+                  .attr("y", yAxis)
+                  .attr("font-size", size)
+                ;
+            }
